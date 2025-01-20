mwhitney57/PiPAA

commit dc70a98d061a4594f53de75e735cddeefcb59f00

Improved loading time for indirect web media.
- Improved speed of indirect web media attribution, thereby improving
load times. Rudimentary testing and measurement yielded ~40% faster
loads with YouTube videos.

src/dev/mwhitney/media/PiPMediaAttributor.java
@@ -198,23 +198,29 @@ public class PiPMediaAttributor implements PropertyListener {
         default -> null;
         };
         
-        final ArrayList<String> preRuns = CFExec.runAndGet(
-            () -> Binaries.execAndFetchSafe(false, Binaries.bin(Bin.YT_DLP), "\"" + src + "\"", "-I", "1", "--print", "\"%(ext)s\""),
-            platSupplier)
-            .excepts((i, e) -> System.err.println("Exception caught from pre-run (#" + i + ") in web attribution: " + e))
-            .results();
-        
+        System.out.println("Attempting web attribution gets...");
+        // Execute multiple binary commands asynchronously and concurrently, including audio-only test, platform-specific and regular attribution.
+        final ArrayList<String> cmdOuts = CFExec.runAndGet(
+                () -> Binaries.execAndFetchSafe(false, Binaries.bin(Bin.YT_DLP), "\"" + src + "\"", "-I", "1", "--print", "\"%(ext)s\""),
+                platSupplier,
+                () -> Binaries.execAndFetchSafe(false, getWebAttributionArgs(src, platform, Bin.YT_DLP,     false).toArray(new String[0])),
+                () -> Binaries.execAndFetchSafe(false, getWebAttributionArgs(src, platform, Bin.GALLERY_DL, false).toArray(new String[0])),
+                () -> Binaries.execAndFetchSafe(false, getWebAttributionArgs(src, platform, Bin.YT_DLP,     true).toArray(new String[0])),
+                () -> Binaries.execAndFetchSafe(false, getWebAttributionArgs(src, platform, Bin.GALLERY_DL, true).toArray(new String[0])))
+                .excepts((i, e) -> System.err.println("Exception caught from binary (#" + i + ") in web attribution: " + e))
+                .results();
+            
         // Check if media is audio first.
-        final String ext = preRuns.get(0);
+        final String ext = cmdOuts.get(0);
         final MediaExt mediaExt = MediaExt.parseSafe(ext);
         final boolean audioMedia = (mediaExt != null && attributeType(mediaExt) == TYPE.AUDIO);
         
         // Platform-specific web media attribution.
-        if (preRuns.get(1) != null) {
+        final String platCMDOutput = cmdOuts.get(1);
+        if (platCMDOutput != null) {
             switch (platform) {
-            case X:
-                final String cmdOutput = preRuns.get(1);
-                final String[] lines = cmdOutput.split("\n");
+            case X -> {
+                final String[] lines = platCMDOutput.split("\n");
                 if (lines.length <= 2 && lines[0].contains("error"))
                     break;
                 
@@ -229,21 +235,24 @@ public class PiPMediaAttributor implements PropertyListener {
                     
                     if (platUser != null && platID != null && platDesc != null) break;
                 }
-                System.out.println("Results [user/id/desc]: " + platUser + "/" + platID + "/" + platDesc);
-                break;
-            default:
-                break;
             }
+            default -> {}
+            }
+//                System.out.println("Plat. Pre-Run WMF Results [user/id/desc]: " + platUser + "/" + platID + "/" + platDesc);    //Debug
         }
         
-        System.out.println("Attempting web attribution gets...");
-        // Determine command arguments based on platform and attempt number.
-        final ArrayList<String> cmdOuts = CFExec.runAndGet(
-              () -> Binaries.execAndFetchSafe(false, getWebAttributionArgs(src, platform, Bin.YT_DLP,     audioMedia).toArray(new String[0])),
-              () -> Binaries.execAndFetchSafe(false, getWebAttributionArgs(src, platform, Bin.GALLERY_DL, audioMedia).toArray(new String[0])))
-              .excepts((i, e) -> System.err.println("Exception caught from binary (#" + i + ") in web attribution: " + e))
-              .results();
-            
+        // Adjust list to only contain the desired outputs.
+        cmdOuts.remove(0);  // Removes extension output used for audio-only test.
+        cmdOuts.remove(0);  // Removes platform-specific attribution output.
+        if (audioMedia) {   // Removes audio-only outputs.
+            cmdOuts.remove(cmdOuts.size() - 1);
+            cmdOuts.remove(cmdOuts.size() - 1);
+        } else {            // Removes regular, non-audio-only outputs.
+            cmdOuts.remove(0);
+            cmdOuts.remove(0);
+        }
+        // List should now be of size() 2.
+        
         // Execute command and retrieve output, splitting by lines.
         for (int cmd = 0; cmd < cmdOuts.size(); cmd++) {
             final boolean usedYTDLP = (cmd == 0);

commit f7e847484e17696bbcd1234c24a85c6262b0da0d

Fixed attribution bug caused by dc70a98 which led to loading failures.
Noticed unintentional inversion of logic in an if statement from commit
dc70a98. This was causing the attributor to pick the wrong media
extension, which then became misaligned with the expected extension
during the download process. The download would ultimately succeed, but
the name would often wrongly include two extensions, and the user would
be told that the media was invalid when it was not. This has been fixed.

src/dev/mwhitney/gui/PiPWindow.java
@@ -1540,7 +1540,7 @@ public class PiPWindow extends JFrame implements PropertyListener, Themed {
         
         // Download Media Using Passed Arguments.
         try {
-            System.out.println("Media DL CMD executing...");
+            System.out.println("Media DL CMD Executing:\n---> " + String.join(" ", args) + "\n");
             Binaries.exec(args);
             System.out.println("Media DL CMD should be done.");
         } catch (IOException | InterruptedException e) { e.printStackTrace(); return null; }

src/dev/mwhitney/media/MediaExt.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import dev.mwhitney.exceptions.InvalidMediaExtensionException;
 import dev.mwhitney.main.PiPEnum;
+import dev.mwhitney.util.PiPAAUtils;
 
 /**
  * File extensions for different media types and containers. All extensions
@@ -109,21 +110,28 @@ public enum MediaExt implements PiPEnum<MediaExt> {
     }
     
     /**
-     * Gets the MediaExt values as a single String, separated by the passed character.
+     * Gets the MediaExt values as a single String, separated by the passed
+     * {@link CharSequence}.
+     * 
+     * @param separator - the {@link CharSequence} to separate each MediaExt value.
+     * @return a String with the values, separated by the passed
+     *         {@link CharSequence}.
+     * @since 0.9.4
+     */
+    public static String values(final CharSequence separator) {
+        return String.join(separator, PiPAAUtils.toStringArray(MediaExt.class));
+    }
+    
+    /**
+     * Gets the MediaExt values as a single String, separated by the passed
+     * character.
      * 
      * @param separator - the char to separate each MediaExt value.
      * @return a String with the values, separated by the passed char.
      * @since 0.9.4
+     * @see {@link #values(CharSequence)} for using multiple separation characters.
      */
-    public static String values(char separator) {
-        final MediaExt[] exts = MediaExt.values();
-        final StringBuilder vals = new StringBuilder();
-        for (int i = 0; i < exts.length; i++) {
-            vals.append(exts[i]);
-            // Only append separator afterward if not last element.
-            if (i < exts.length - 1)
-                vals.append(separator);
-        }
-        return vals.toString();
+    public static String values(final char separator) {
+        return values(String.valueOf(separator));
     }
 }

src/dev/mwhitney/media/PiPMediaAttributor.java
@@ -288,12 +288,12 @@ public class PiPMediaAttributor implements PropertyListener {
         // Adjust list to only contain the desired outputs.
         cmdOuts.remove(0);  // Removes extension output used for audio-only test.
         cmdOuts.remove(0);  // Removes platform-specific attribution output.
-        if (audioMedia) {   // Removes audio-only outputs.
-            cmdOuts.remove(cmdOuts.size() - 1);
-            cmdOuts.remove(cmdOuts.size() - 1);
-        } else {            // Removes regular, non-audio-only outputs.
+        if (audioMedia) {   // Removes regular, non-audio-only outputs.
             cmdOuts.remove(0);
             cmdOuts.remove(0);
+        } else {            // Removes audio-only outputs.
+            cmdOuts.remove(cmdOuts.size() - 1);
+            cmdOuts.remove(cmdOuts.size() - 1);
         }
         // List should now be of size() 2.
         
@@ -437,8 +437,8 @@ public class PiPMediaAttributor implements PropertyListener {
                     webArgs.add("-S");
                     webArgs.add("aext");
                 }
-//                platformArgs.add("--ffmpeg-location");
-//                platformArgs.add(Binaries.FFMPEG_LOC_ARG);
+                webArgs.add("--ffmpeg-location");       // yt-dlp can use to help select the best format.
+                webArgs.add(Binaries.FFMPEG_LOC_ARG);
                 webArgs.add("--no-playlist");
                 webArgs.add("-I");
                 webArgs.add("1");
@@ -457,6 +457,7 @@ public class PiPMediaAttributor implements PropertyListener {
             break;
         }
         
+//        System.out.println("Media Attribution Web Args:\n---> " + String.join(" ", webArgs) + "\n");    // Debug
         return webArgs;
     }
     

src/dev/mwhitney/util/PiPAAUtils.java
@@ -2,6 +2,7 @@ package dev.mwhitney.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -41,6 +42,15 @@ public class PiPAAUtils {
      */
     public static String slashFix(final String path) {
         return path.replace('/', '\\');
+    }
+    
+    /**
+     * Gets an array this enum's values as String objects.
+     * 
+     * @return a String[] of this enum's values.
+     */
+    public static String[] toStringArray(final Class<? extends Enum<?>> e) {
+        return Arrays.stream(e.getEnumConstants()).map(Enum::toString).toArray(String[]::new);
     }
     
     /**