df925e262db2df9aa6b4e5fd056c9bd203804d3a
Matt Whitney <175911926+mwhitney57@users.noreply.github.com>
2025-10-10 19:48:49 -0400

    Code improvements for FadingLineBorder used on windows.
    Improved documentation, consistency, redundancy, and performance cost.

--- src/dev/mwhitney/gui/decor/FadingLineBorder.java
+++ src/dev/mwhitney/gui/decor/FadingLineBorder.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
 
 import javax.swing.Timer;
 import javax.swing.border.LineBorder;
@@ -13,31 +14,32 @@ import dev.mwhitney.listeners.PaintRequester;
 
 /**
  * A simple extension upon the {@link LineBorder} class with the capability to
- * fade its Color into transparency upon request.
+ * fade its {@link Color} into transparency upon request.
  * 
  * @author mwhitney57
  */
 public class FadingLineBorder extends LineBorder implements PaintRequester {
-    /** The randomly-generated serial UID for the FadingLineBorder class. */
+    /** The randomly-generated serial UID. */
     private static final long serialVersionUID = -960971844424714747L;
 
     /** The default {@link Color} for the line border. */
     private static final Color COLOR_DEFAULT = new Color(255, 0, 0, 200);
     
-    /** The repeating Timer used for the border fading process which stops once the border has faded. */
-    private Timer fadeTimer;
-    /** The Color when the border is completely normal and not faded. */
-    private Color colorNormal;
+    /** The repeating {@link javax.swing.Timer} used for the border fading process which stops once the border has faded. */
+    private final Timer fadeTimer = new Timer(10, this::handleFadeTimerEvent);
+    /** The {@link Color} when the border is completely normal and not faded. */
+    private final Color colorNormal;
     
     /**
-     * Creates a new FadingLineBorder, which is a simple extension upon
+     * Creates a new {@link FadingLineBorder} which is a simple extension upon
      * {@link LineBorder} with the capability to fade its Color into transparency
-     * upon request. This constructor automatically enables the rounding of the
-     * border. Additionally, it enables antialiasing when drawing the border for a
-     * cleaner look.
+     * upon request.
+     * <p>
+     * This border has rounded corners by default. It also paints using
+     * anti-aliasing for a cleaner look.
      * 
-     * @param color     - the initial/normal Color of the border when it is fully
-     *                  visible and fading has not begun.
+     * @param color     - the initial/normal {@link Color} of the border when it is
+     *                  fully visible and fading has not begun.
      * @param thickness - an int with the thickness of the border in pixels.
      */
     public FadingLineBorder(Color color, int thickness) {
@@ -46,22 +48,6 @@ public class FadingLineBorder extends LineBorder implements PaintRequester {
         // Save normal color and its faded counterpart, then set color to faded.
         this.colorNormal = color;
         this.lineColor   = fadedOf(color);
-        
-        fadeTimer = new Timer(10, (e) -> {
-            // Apply Default Color if null
-            if (this.lineColor == null)
-                this.lineColor = COLOR_DEFAULT;
-            // Stop Timer if Alpha has Reached 0 (Faded)
-            if (this.lineColor.getAlpha() == 0) {
-                ((Timer) e.getSource()).stop();
-                return;
-            }
-            
-            // Adjust the border line color's transparency value and request a repaint.
-            final int r = lineColor.getRed(), g = lineColor.getGreen(), b = lineColor.getBlue(), a = lineColor.getAlpha();
-            this.lineColor = new Color(r, g, b, Math.max(0, a - 2));
-            requestPaint();
-        });
     }
     
     /*
@@ -84,61 +70,86 @@ public class FadingLineBorder extends LineBorder implements PaintRequester {
         if (!isFaded()) {
             // Enable anti-aliasing, paint the rounded border, then disable anti-aliasing.
             // If not disabled, the anti-aliasing will persist to other components and cause a massive hit to performance.
-            ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            final Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             super.paintBorder(c, g, x, y, width, height);
-            ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
         }
     }
     
     /**
-     * Checks if this FadingLineBorder is faded, meaning it is fully transparent.
+     * Handles an event firing from the Swing {@link #fadeTimer}. Each call of this
+     * method works to fade the border further, until its alpha value has reached
+     * zero. At that point, the border is fully faded and transparent, so the timer
+     * will be stopped.
+     * 
+     * @param e - the {@link ActionEvent} supplied by the timer.
+     */
+    private void handleFadeTimerEvent(ActionEvent e) {
+        // Apply Default Color if null
+        if (this.lineColor == null)
+            this.lineColor = COLOR_DEFAULT;
+        // Stop Timer if Alpha has Reached 0 (Faded)
+        if (isFaded()) {
+            fadeTimer.stop();
+            return;
+        }
+        
+        // Adjust the border line color's transparency value and request a repaint.
+        final int r = lineColor.getRed(), g = lineColor.getGreen(), b = lineColor.getBlue(), a = lineColor.getAlpha();
+        this.lineColor = new Color(r, g, b, Math.max(0, a - 2));
+        requestPaint();
+    }
+    
+    /**
+     * Checks if this {@link FadingLineBorder} is faded, meaning it is fully
+     * transparent.
      * 
      * @return <code>true</code> if the border is fully transparent;
      *         <code>false</code> otherwise.
      */
     public boolean isFaded() {
-        return (lineColor.getAlpha() == 0);
+        return this.lineColor.getAlpha() == 0;
     }
     
     /**
-     * Fades this FadingLineBorder, starting the process from its initial base color
-     * and fading it over time. The fading process is complete and stops once the
-     * border is fully transparent.
+     * Fades this {@link FadingLineBorder}, starting the process from its initial
+     * base color and fading it over time. The fading process is complete and stops
+     * once the border is fully transparent.
      */
     public void fade() {
-        fade(colorNormal);
+        fade(null);
     }
     
     /**
-     * Fades this FadingLineBorder, starting the process from the passed Color and
-     * fading it over time. The fading process is complete and stops once the border
-     * is fully transparent. Since the fading process is constant over time, passing
-     * a more or less transparent Color will impact the amount of time it takes to
-     * fade.
+     * Fades this {@link FadingLineBorder}, starting the process from the passed
+     * Color and fading it over time. The fading process is complete and stops once
+     * the border is fully transparent.
+     * <p>
+     * Since the fading process is constant over time, passing a more or less
+     * transparent {@link Color} will impact the amount of time it takes to fade.
      * 
-     * @param c - the base Color to start with before starting the fade.
+     * @param c - the base {@link Color} to start with before starting the fade.
      */
     public void fade(final Color c) {
-        if (c != null) {
-            this.lineColor = c;
-        } else
-            this.lineColor = colorNormal;
+        this.lineColor = (c != null ? c : colorNormal);
         fadeTimer.start();
     }
     
     /**
-     * Calculates and returns a faded (transparent) version of the passed Color.
+     * Calculates and returns a faded (transparent) version of the passed
+     * {@link Color}.
      * 
-     * @param c - the Color to get a faded version of.
-     * @return the passed Color, but faded (transparent.)
+     * @param c - the {@link Color} to get a faded version of.
+     * @return the passed {@link Color}, but faded (transparent).
      */
     private Color fadedOf(final Color c) {
         return (new Color(c.getRed(), c.getGreen(), c.getBlue(), 0));
     }
     
     /**
-     * Cancels any ongoing fading process. This method will then automatically fade
-     * the border, restoring its transparency.
+     * Cancels any ongoing fading process. This method then automatically sets the
+     * border to a faded state, restoring its transparency.
      */
     public void cancelFade() {
         fadeTimer.stop();
